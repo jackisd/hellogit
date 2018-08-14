c2a7687a685e0ddf1e8e4883f5a0f7bf473951ac
Administrator <Administrator@Jack>
2018-08-14 21:54:27 +0800

    change

--- src/com/jack/test/Demo.java
+++ src/com/jack/test/Demo.java
@@ -2,6 +2,8 @@ package com.jack.test;
 
 public class Demo {
 	public static void main(String[] args) {
+		String string = "Hello GitHub";
+		System.out.println("Hello GitHub");
 		System.out.println("Hello GitHub 随便改一改 h");
 	}
 }
