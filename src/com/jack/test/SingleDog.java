d5cb4f00156740add61df2edcfe75556802772e6
Administrator <Administrator@Jack>
2018-08-17 22:22:32 +0800

    2018年七夕

---
+++ src/com/jack/test/SingleDog.java
@@ -0,0 +1,18 @@
+package com.jack.test;
+
+//饿汉式单例，
+public class SingleDog {
+	private static SingleDog dog = new SingleDog();
+
+	public SingleDog() {
+		System.out.println("2018年七夕，单身节快乐。");
+	}
+
+	public static SingleDog get() {
+		return dog;
+	}
+	public static void main(String[] args) {
+		System.out.println(get());
+		System.out.println(get());
+	}
+}
